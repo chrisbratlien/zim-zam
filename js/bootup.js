f72c5aa8f07584fc21aded71d6d65e50606b60f3
Chris Bratlien <c-bratlien@tti.tamu.edu>
2017-06-23 13:54:12 -0500

    trades, import

--- js/bootup.js
+++ js/bootup.js
@@ -4,3 +4,23 @@ BSD.remoteStorage = BSD.RemoteStorage({
 });
 
 var campfire = BSD.PubSub({});
+
+
+BSD.import = function (url, success,error) {
+  jQuery.ajax({
+    type : 'GET',
+    url : url,
+    success: success,
+    error: error
+  });
+};
+
+BSD.importJSON = function(url, success,error) {
+  jQuery.ajax({
+    type: 'GET',
+    url: url,
+    dataType: 'json',
+    success: success,
+    error: error
+  });
+};
